5ed83b572b09157c90ee56e410010baed95f0171
HaythemLazaar <haythem.lazaar1@gmail.com>
2023-10-10 13:08:17 +0100

    collo project link

--- src/app/(pages)/collo/page.tsx
+++ src/app/(pages)/collo/page.tsx
@@ -7,7 +7,7 @@ export default function ProjectCollo() {
   return (
     <div>
       <ProjectDescription
-        title="Collo: Where teams work at maximum productivity"
+        title="Collo"
         subtitle="Project Management, Communication & Collaboration Platform."
         role="Product Designer & Software Engineer"
         client="Esprit"
@@ -17,6 +17,7 @@ export default function ProjectCollo() {
           "The initial phase involved a thorough cleanup of the outdated UI, followed by the creation of a fresh design system that seamlessly incorporated the app's branding elements. ",
           "Leveraging cutting-edge technologies, we used next.js in a Service-oriented architecture with authentication using NextAuth, a seperate emailing service and a core functionality service in Nest.js along real time video chat using Twilio SDK.",
         ]}
+        link="https://www.collo.app"
       />
       <Divider />
       <div className="w-full flex flex-col items-center">
